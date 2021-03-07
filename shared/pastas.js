5076df285f1b96b41fa5081dd6cad1cf2e5b4d56
MARJORIE CICERON <jorie@marjories-mbp.myfiosgateway.com>
2021-03-07 17:36:38 -0500

    Navigation Part 3

--- shared/pastas.js
+++ shared/pastas.js
@@ -4,34 +4,34 @@ export const PASTAS= [
         name: "Penne",
         image: "/components/images/4dc2327eb71687efbb55551e25812343.jpg",
         feature: false,
-        descrription: "Penne is one of the most famous Italian pasta shapes and is loved across Italy and the world. Penne translates to the word “pen”, and gets its name from its shape – a short cylinder-shaped pasta that has angled edges -pairs verry well with chuncky meat and frequently used in baked casserole dishes",
+        description: "Penne is one of the most famous Italian pasta shapes and is loved across Italy and the world. Penne translates to the word “pen”, and gets its name from its shape – a short cylinder-shaped pasta that has angled edges -pairs verry well with chuncky meat and frequently used in baked casserole dishes",
     },
     {
         id: 1,
         name: "Spaghetti with Meatballs",
         image: "/components/images/Spaghetti-and-Meatballs-SpendWithPennies-4.jpg",
         feature: false,
-        descrription: "Spaghetti is the most popular shape in Italy. The name comes from the Italian word spaghi, which means lengths of cord.",
+        description: "Spaghetti is the most popular shape in Italy. The name comes from the Italian word spaghi, which means lengths of cord.",
     },
     {
         id: 2,
         name: "Linguine",
         image: "/components/images/ecbf563c4a09a3cc6bab1b20470c09ec.jpg",
         feature: false,
-        descrription: "Linguine is a type of pasta similar to fettuccine and trenette but elliptical in section rather than flat",
+        description: "Linguine is a type of pasta similar to fettuccine and trenette but elliptical in section rather than flat",
     },
     {
         id: 3,
         name: "Stuffed Shells",
         image: "/components/images/ricotta-stuffed-shells-1.jpg",
         feature: false,
-        descrription: " Stuffed shells are made with conchiglioni pasta, the larger version of conchiglie, which means “conch shells. They're usually stuffed with a combination of meat, cheeses, and vegetables.",
+        description: " Stuffed shells are made with conchiglioni pasta, the larger version of conchiglie, which means “conch shells. They're usually stuffed with a combination of meat, cheeses, and vegetables.",
     },{
         id: 4,
         name: "Lasagna",
         image: "/components/images/Best-Lasagna-550-500x500.jpg",
         feature: true,
-        descrription: "Lasagna is a baked Italian dish consisting of wide strips of pasta cooked and layered with meat, vegetables, cheese, and tomato sauce",
+        description: "Lasagna is a baked Italian dish consisting of wide strips of pasta cooked and layered with meat, vegetables, cheese, and tomato sauce",
        
     }
 
